6f1787dd19b4c5356f116ad4cd407252fdf99af6
imannig <zahranansaria@gmail.com>
2022-08-25 20:49:58 +0700

    spes: Auto clone vt, kt, and hardware hals
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,13 @@ sleep 1
 
 # Remove pixel headers to avoid conflicts
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
+
+# Remove hardware/xiaomi
+rm -rf hardware/xiaomi
+
+# Clone hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-19.1
+
+# Clone trees
+git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_spes vendor/xiaomi/spes
+git clone https://github.com/PixelPlusUI-Devices/android_kernel_xiaomi_spes-header --depth=1 kernel/xiaomi/spes
